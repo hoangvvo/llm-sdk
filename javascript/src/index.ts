d789546e4058dd599e29df2348811e0494d73cfc
Hoang Vo <dev+g@hvvo.dev>
2024-10-25 06:23:30 +0700

    version: 0.1.0

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,4 +1,4 @@
 import * as schemas from "./schemas/schemas.gen.js";
-export type { LanguageModel } from "./models/language-model.js";
+export * from "./models/language-model.js";
 export * from "./schemas/types.gen.js";
 export { schemas };
