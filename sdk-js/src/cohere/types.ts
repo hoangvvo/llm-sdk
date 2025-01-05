1a609fbf0ebdaac1e711dc842966f10d9dc70bbc
Hoang Vo <dev+g@hvvo.dev>
2025-01-05 16:04:20 +0700

    chore: cleanup

--- sdk-js/src/cohere/types.ts
+++
@@ -1,8 +0,0 @@
-export interface CohereModelOptions {
-  apiKey: string;
-  modelId: string;
-  /**
-   * If the AudioPart has a transcript, convert it to an TextPart
-   */
-  convertAudioPartsToTextParts?: boolean;
-}
